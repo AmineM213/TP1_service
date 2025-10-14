f6b55c3a86740b85052231e71585a6fb31ca42b4
AmineM <mezheriamine@gmail.com>
2025-10-13 22:53:36 -0400

    ecriture du code main.java

---
+++ src/main/java/com/hautlesbas/Main.java
@@ -0,0 +1,27 @@
+package com.hautlesbas;
+
+import com.hautlesbas.service.ServiceInventaire;
+import com.hautlesbas.service.ServiceVente;
+import com.hautlesbas.api.ChaussetteHandler;
+import com.hautlesbas.api.VenteHandler;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ServiceInventaire serviceInventaire = new ServiceInventaire();
+        ServiceVente serviceVente = new ServiceVente(serviceInventaire);
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/chaussettes", new ChaussetteHandler(serviceInventaire));
+        server.createContext("/ventes", new VenteHandler(serviceVente));
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Serveur démarré sur le port 8080");
+        System.out.println("API Chaussettes disponible sur: http://localhost:8080/chaussettes");
+        System.out.println("API Ventes disponible sur: http://localhost:8080/ventes");
+    }
+}
